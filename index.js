ba7c588a602b0b85132effc65c96d1e57d8f1908
Alex-346 <webkotoba@gmail.com>
2024-07-26 15:22:55 +0200

    Added typewriter plug-in and basic js form handling

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+function generatePoem(event) {
+  event.preventDefault();
 
+  new Typewriter("#poem", {
+    strings:
+      "A world of dew, and within every dewdrop, a world of struggle. By Kobayashi Issa",
+    autoStart: true,
+    cursor: null,
+    delay: 40,
+  });
+}
+
+poemFormElement = document.querySelector("#poem-instruction-form");
+poemFormElement.addEventListener("submit", generatePoem);
